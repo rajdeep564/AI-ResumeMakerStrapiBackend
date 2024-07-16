64faea69c6df58e9f5f1e0841b679d26f28793e7
rajdeep564 <rajdeepsinghchavda7@gmail.com>
2024-07-16 16:41:53 +0530

    commit

---
+++ types/generated/components.d.ts
@@ -0,0 +1,56 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface EducationEducation extends Schema.Component {
+  collectionName: 'components_education_educations';
+  info: {
+    displayName: 'Education';
+    icon: 'book';
+  };
+  attributes: {
+    universityName: Attribute.String;
+    degree: Attribute.String;
+    major: Attribute.String;
+    startDate: Attribute.String;
+    endDate: Attribute.String;
+    description: Attribute.Text;
+  };
+}
+
+export interface ExperienceExperience extends Schema.Component {
+  collectionName: 'components_experience_experiences';
+  info: {
+    displayName: 'Experience';
+    icon: 'archive';
+  };
+  attributes: {
+    title: Attribute.String;
+    companyName: Attribute.String;
+    city: Attribute.String;
+    state: Attribute.String;
+    startDate: Attribute.String;
+    endDate: Attribute.String;
+    workSummery: Attribute.RichText;
+  };
+}
+
+export interface SkillsSkills extends Schema.Component {
+  collectionName: 'components_skills_skills';
+  info: {
+    displayName: 'Skills';
+    icon: 'star';
+  };
+  attributes: {
+    name: Attribute.String;
+    rating: Attribute.Integer;
+  };
+}
+
+declare module '@strapi/types' {
+  export module Shared {
+    export interface Components {
+      'education.education': EducationEducation;
+      'experience.experience': ExperienceExperience;
+      'skills.skills': SkillsSkills;
+    }
+  }
+}
